4b2a9d09615675a66b66307f62da1ca7fc184d96
developer <prodeveloper.py@gmail.com>
2026-07-30 14:37:16 +0500

    Deploy prebuilt static website without Node

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-echo "Installing production dependencies..."
-npm ci
-
-echo "Building the UpForge website..."
-npm run deploy
-
 DEPLOY_PATH="${HOME}/public_html"
 echo "Publishing dist to ${DEPLOY_PATH}..."
 mkdir -p "${DEPLOY_PATH}"
